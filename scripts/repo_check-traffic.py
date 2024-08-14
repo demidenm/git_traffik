743dbb364be843424b62357c80d5adb30c285a92
Michael Demidenko <36335002+demidenm@users.noreply.github.com>
2024-08-14 15:22:57 -0700

    resolve workflow error

--- scripts/repo_check-traffic.py
+++ scripts/repo_check-traffic.py
@@ -11,8 +11,9 @@ owner = os.getenv('OWNER')
 repo = os.getenv('REPO')
 token = os.getenv('MY_ACCESS_TOKEN')
 
-# Output data
-csv_file = f'output/{repo}_git-trafficdata.csv'
+# Define the output directory and CSV file path
+output_dir = 'output'
+csv_file = os.path.join(output_dir, f'{repo}_git-trafficdata.csv')
 
 views_url = f'https://api.github.com/repos/{owner}/{repo}/traffic/views'
 clones_url = f'https://api.github.com/repos/{owner}/{repo}/traffic/clones'
@@ -20,7 +21,6 @@ clones_url = f'https://api.github.com/repos/{owner}/{repo}/traffic/clones'
 headers = {
     'Authorization': f'token {token}'
 }
-
 
 # Function to get data from a given git URL
 def get_data(url):
@@ -31,9 +31,8 @@ def get_data(url):
         print(f"Failed to get data: {response.status_code} - {response.text}")
         return None
 
-
 # Create output folder if it does not exist
-os.makedirs('output', exist_ok=True)
+os.makedirs(output_dir, exist_ok=True)
 
 # Get existing data if CSV file exists
 if os.path.exists(csv_file):
@@ -57,8 +56,9 @@ if clones_data:
     clones_df['timestamp'] = pd.to_datetime(clones_df['timestamp'])
     clones_df = clones_df.rename(columns={'count': 'clones_count', 'uniques': 'clones_uniques'})
 
+# Merge views and clones dataframes
 merged_viewsclones = pd.merge(views_df, clones_df, on='timestamp', how='outer')
-# After outer join, fill na's with zeros
+# After outer join, fill NA values with zeros
 merged_viewsclones = merged_viewsclones.fillna(0)
 
 # Save updated traffic_df to CSV
@@ -67,16 +67,15 @@ concatenated_df['timestamp'] = concatenated_df['timestamp'].astype(str).str.spli
 uniq_df = concatenated_df.drop_duplicates(subset=['timestamp'])
 uniq_df.to_csv(csv_file, index=False)
 
-
 # Create plots
 
-## Set Seaborn style and context
+# Set Seaborn style and context
 sns.set(style='whitegrid', font='Arial', rc={"axes.labelsize": 14, "xtick.labelsize": 12, "ytick.labelsize": 12})
 
-## Create a figure with subplots
+# Create a figure with subplots
 plt.figure(figsize=(12, 8))
 
-## Plot views_count and views_uniques
+# Plot views_count and views_uniques
 plt.subplot(2, 1, 1)
 plt.plot(uniq_df['timestamp'], uniq_df['views_count'], label='Views Count')
 plt.plot(uniq_df['timestamp'], uniq_df['views_uniques'], label='Views Uniques')
@@ -86,7 +85,7 @@ plt.ylabel('Count', fontsize=14)
 plt.xticks(rotation=90)
 plt.legend()
 
-## Plot clones_count and clones_uniques
+# Plot clones_count and clones_uniques
 plt.subplot(2, 1, 2)
 plt.plot(uniq_df['timestamp'], uniq_df['clones_count'], label='Clones Count')
 plt.plot(uniq_df['timestamp'], uniq_df['clones_uniques'], label='Clones Uniques')
@@ -96,7 +95,7 @@ plt.ylabel('Count', fontsize=14)
 plt.xticks(rotation=90)
 plt.legend()
 
-## Save plot
+# Save plot
 plt.tight_layout()
-plt.savefig(f'output/{repo}_traffic-data.png', dpi=300)
+plt.savefig(os.path.join(output_dir, f'{repo}_traffic-data.png'), dpi=300)
 plt.close()
